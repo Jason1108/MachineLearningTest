2bcd60a06d199e388ec50d5c0a6661cf8f9ebb22
zzhang2 <zhangze2959569@163.com>
2017-06-07 19:51:40 +0800

    test.py add code

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-import numpy
-import scipy
-import matplotlib
-import sklearn
+# import numpy
+# import scipy
+# import matplotlib
+# import sklearn
+
+from sklearn.datasets import load_boston
+boston = load_boston()
+print(boston.data.shape)
+# (506, 13)
+
+data, target = load_boston(return_X_y=True)
+print(data.shape)
+# (506, 13)
+print(target.shape)
+# (506,)
