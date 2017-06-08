ffff1345ac5cfe3d576ca226b76cea77240fdd3d
Zhang Ze <zhangze2959569@163.com>
2017-06-09 00:19:54 +0800

    update

--- kmeans_pic.py
+++ kmeans_pic.py
@@ -1,3 +1,14 @@
 #coding:utf8
 
 import PIL.Image as image
+
+file_path = 'data/bull.jpg'
+f = open(file_path, 'rb')
+data = []
+img = image.open(f)
+m, n = img.size
+for i in range(m):
+    for j in range(n):
+        x, y, z = img.getpixel((i, j))
+        data.append([x / 256.0, y / 256.0, z / 256.0])
+f.close()
